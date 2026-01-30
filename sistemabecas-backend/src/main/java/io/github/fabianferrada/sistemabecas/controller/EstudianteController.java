220d74ec77bfd6e1d3620ecbe982cd673000bf73
Fabián Ferrada <fabianferrada406@gmail.com>
2026-01-30 18:07:30 -0300

    Added web starter dep to backend

---
+++ sistemabecas-backend/src/main/java/io/github/fabianferrada/sistemabecas/controller/EstudianteController.java
@@ -0,0 +1,15 @@
+package io.github.fabianferrada.sistemabecas.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/estudiante")
+public class EstudianteController {
+	@GetMapping("/{id}")
+	public String obtenerPorId(@PathVariable int id) {
+		return id;
+	}
+}
