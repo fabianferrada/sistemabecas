09ea1ddea51a0c723d874acdb05f235a980fc19e
Fabián Ferrada <fabianferrada406@gmail.com>
2026-01-30 16:11:26 -0300

    Added base Spring Boot files

---
+++ sistemabecas/src/main/java/io/github/fabianferrada/sistemabecas/SistemabecasApplication.java
@@ -0,0 +1,13 @@
+package io.github.fabianferrada.sistemabecas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemabecasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemabecasApplication.class, args);
+	}
+
+}
---
+++ sistemabecas/src/test/java/io/github/fabianferrada/sistemabecas/SistemabecasApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.fabianferrada.sistemabecas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemabecasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
