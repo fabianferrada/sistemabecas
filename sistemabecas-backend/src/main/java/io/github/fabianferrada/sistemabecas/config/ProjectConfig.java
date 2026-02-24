84578723ff1bfb15cd433c0cdb38eef6a02c7093
Fabián Ferrada <fabianferrada406@gmail.com>
2026-02-24 16:15:58 -0300

    Added Spring Security config for backend (OAuth2 resource server)

---
+++ sistemabecas-backend/src/main/java/io/github/fabianferrada/sistemabecas/config/ProjectConfig.java
@@ -0,0 +1,53 @@
+package io.github.fabianferrada.sistemabecas.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.spec.SecretKeySpec;
+
+@Configuration
+public class ProjectConfig {
+	@Value("${tokens.jwt-token}")
+	private String keystr;
+	
+	public ProjectConfig() {}
+	
+	@Bean
+	NimbusJwtDecoder jwtDecoder() {
+		byte key[] = keystr.getBytes();
+		SecretKeySpec secretKey = new SecretKeySpec(key, "HmacSHA256");
+		
+		return NimbusJwtDecoder.withSecretKey(secretKey).build();
+	}
+	
+	@Bean
+	JwtAuthenticationProvider jwtAuthProvider(NimbusJwtDecoder decoder) {
+		return new JwtAuthenticationProvider(decoder);
+	}
+	
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthenticationProvider authProvider) throws Exception {
+		http.authenticationProvider(authProvider);
+		
+		http.authorizeHttpRequests(
+			c -> c.requestMatchers(
+				"/api/apelacion/**",
+				"/api/beca/**",
+				"/api/docpostulacion/**",
+				"/api/documentos/**",
+				"/api/estudiante/**",
+				"/api/postulacion/**"
+			).hasAuthority("SCOPE_estudiante")
+			.requestMatchers("/api/administrador/**").hasAuthority("SCOPE_administrador")
+		)
+			.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+		
+		return http.build();
+	}
+}
