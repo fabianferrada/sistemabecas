0e6b6f55b7ce575ca852541919cded1cd4ca1afb
Fabian Ferrada <fabian.ferrada2001@alumnos.ubiobio.cl>
2026-02-27 18:28:02 -0300

    Added JWT generation to Auth service

--- sistemabecas-auth/src/main/java/io/github/fabianferrada/sistemabecas_auth/controller/EstudianteController.java
+++ sistemabecas-auth/src/main/java/io/github/fabianferrada/sistemabecas_auth/controller/EstudianteController.java
@@ -1,9 +1,13 @@
 package io.github.fabianferrada.sistemabecas_auth.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import javax.crypto.spec.SecretKeySpec;
 
 import io.github.fabianferrada.sistemabecas_auth.dto.LoginDto;
 import io.github.fabianferrada.sistemabecas_auth.dto.StandardResponse;
@@ -24,25 +29,46 @@ public class EstudianteController {
 	@Autowired
 	private EstudianteRepository estudianteRepository;
 	
+	private NimbusJwtEncoder jwtEncoder;
+	
 	private AuthenticationManager authManager;
 	
-	public EstudianteController(AuthenticationManager authManager) {
+	public EstudianteController(
+		AuthenticationManager authManager,
+		@Value("${tokens.jwt-token}") String jwtToken
+	) {
 		this.authManager = authManager;
+		
+		SecretKeySpec secretKey = new SecretKeySpec(
+			jwtToken.getBytes(),
+			"Hmac256"
+		);
+		
+		jwtEncoder = NimbusJwtEncoder.withSecretKey(
+			secretKey
+		).build();
 	}
 	
 	@PostMapping("/login")
-	public @ResponseBody StandardResponse login(@RequestBody LoginDto loginDto) {
+	public @ResponseBody String login(@RequestBody LoginDto loginDto) {
 		UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
 			loginDto.email,
 			loginDto.password
 		);
+		
 		try {
 			authManager.authenticate(token);
 		} catch (AuthenticationException e) {
-			return new StandardResponse(false);
+			return null;
 		}
 		
-		return new StandardResponse(true);
+		return jwtEncoder.encode(
+			JwtEncoderParameters.from(
+				JwtClaimsSet.builder()
+					.claim("scope", "estudiante")
+					.build()
+			)
+		).getTokenValue();
 	}
 	
 	@PostMapping("/registrar")
