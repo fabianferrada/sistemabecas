4cb63f9178ffacd441e9085193fc3fb6fe8fce62
Fabian Ferrada <fabian.ferrada2001@alumnos.ubiobio.cl>
2026-03-06 17:13:47 -0300

    Added edit profile component

--- sistemabecas-frontend/src/app/services/globalstate.ts
+++ sistemabecas-frontend/src/app/services/globalstate.ts
@@ -13,5 +13,30 @@ export enum State {
   providedIn: 'root',
 })
 export class Globalstate {
-  currentState: State = State.Login;
+	currentState: State = State.Login;
+
+	public goLogin() {
+		// TODO
+		this.currentState = State.Login;
+	}
+	
+	public goProfile() {
+		// TODO
+		this.currentState = State.User;
+	}
+	
+	public goApplications() {
+		// TODO
+		this.currentState = State.Applications;
+	}
+	
+	public goScholarships() {
+		// TODO
+		this.currentState = State.Scholarships;
+	}
+	
+	public goAppeals() {
+		// TODO
+		this.currentState = State.Appeals;
+	}
 }
--- sistemabecas-frontend/src/app/student/navbar/navbar.ts
+++ sistemabecas-frontend/src/app/student/navbar/navbar.ts
@@ -11,29 +11,4 @@ export class StudentNavbar {
 	State = State;
 	
 	constructor(public globalState: Globalstate) {}
-	
-	public goLogin() {
-		// TODO
-		this.globalState.currentState = State.Login;
-	}
-	
-	public goProfile() {
-		// TODO
-		this.globalState.currentState = State.User;
-	}
-	
-	public goApplications() {
-		// TODO
-		this.globalState.currentState = State.Applications;
-	}
-	
-	public goScholarships() {
-		// TODO
-		this.globalState.currentState = State.Scholarships;
-	}
-	
-	public goAppeals() {
-		// TODO
-		this.globalState.currentState = State.Appeals;
-	}
 }
