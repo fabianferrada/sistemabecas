88ce81d2988230563aaa3c8dad2972e9399f3155
Fabian Ferrada <fabian.ferrada2001@alumnos.ubiobio.cl>
2026-03-06 00:54:29 -0300

    Added login component

--- sistemabecas-frontend/src/app/app.ts
+++ sistemabecas-frontend/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { StudentLogin } from './student/login/login';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ StudentLogin ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ sistemabecas-frontend/src/app/student/login/login.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'student-login',
+  imports: [],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class StudentLogin {
+
+}
