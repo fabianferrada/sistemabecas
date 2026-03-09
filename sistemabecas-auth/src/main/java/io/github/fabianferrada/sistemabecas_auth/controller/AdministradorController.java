98402238534fb78aec75b96e744ed21cc54512e3
Fabian Ferrada <fabianferrada406@gmail.com>
2026-03-09 14:55:31 -0300

    Added admin login

--- sistemabecas-auth/src/main/java/io/github/fabianferrada/sistemabecas_auth/controller/AdministradorController.java
+++ sistemabecas-auth/src/main/java/io/github/fabianferrada/sistemabecas_auth/controller/AdministradorController.java
@@ -1,15 +1,23 @@
 package io.github.fabianferrada.sistemabecas_auth.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import io.github.fabianferrada.sistemabecas_auth.model.Administrador;
+import javax.crypto.spec.SecretKeySpec;
+
+import io.github.fabianferrada.sistemabecas_auth.dto.LoginDto;
 import io.github.fabianferrada.sistemabecas_auth.repository.AdministradorRepository;
 
 @Controller
@@ -18,16 +26,45 @@ public class AdministradorController {
 	@Autowired
 	private AdministradorRepository administradorRepository;
 	
-	@GetMapping("/obtener/{id}")
-	public @ResponseBody Administrador obtenerAdministrador(@PathVariable int id) {
-		return administradorRepository.findById(id).orElse(null);
+	private NimbusJwtEncoder jwtEncoder;
+	
+	private AuthenticationManager authManager;
+	
+	public AdministradorController(
+		AuthenticationManager authManager,
+		@Value("${tokens.jwt-token}") String jwtToken
+	) {
+		this.authManager = authManager;
+		
+		SecretKeySpec secretKey = new SecretKeySpec(
+			jwtToken.getBytes(),
+			"Hmac256"
+		);
+		
+		jwtEncoder = NimbusJwtEncoder.withSecretKey(
+			secretKey
+		).build();
 	}
 	
-	@PutMapping("/actualizar/{id}")
-	public @ResponseBody Administrador actualizarAdministrador(
-		@PathVariable int id, @RequestBody Administrador administrador
-	) {
-		administradorRepository.deleteById(id);
-		return administradorRepository.save(administrador);
+	@PostMapping("/login")
+	public @ResponseBody String login(@RequestBody LoginDto loginDto) {
+		UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+			loginDto.email,
+			loginDto.password
+		);
+		
+		try {
+			authManager.authenticate(token);
+		} catch (AuthenticationException e) {
+			return null;
+		}
+		
+		return jwtEncoder.encode(
+			JwtEncoderParameters.from(
+				JwtClaimsSet.builder()
+				.claim("scope", "administrador")
+				.build()
+			)
+		).getTokenValue();
 	}
 }
