43e0693784501ba52871b4754bbd8c4811924c8b
Fabian Ferrada <fabianferrada406@gmail.com>
2026-03-20 13:48:31 -0300

    Added form modules to studentcomponent

--- sistemabecas-frontend/src/app/student/applications/applications.ts
+++ sistemabecas-frontend/src/app/student/applications/applications.ts
@@ -1,20 +1,24 @@
 import { Component } from '@angular/core';
 import { Applications } from '../../services/student/applications';
-import { Globalstate } from '../../services/globalstate';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'student-applications',
-  imports: [],
+  imports: [ FormsModule ],
   templateUrl: './applications.html',
   styleUrl: './applications.css',
 })
 export class StudentApplications {
-	constructor(public globalState: Globalstate, public applicationsService: Applications) {}
+	public razonApelacion: string = "";
+	public descripcionApelacion: string = "";
+	
+	constructor(public applicationsService: Applications) {}
 	
 	public capitalize(text: string) {
 		return text[0].toUpperCase() + text.substr(1).toLowerCase();
 	}
 	
 	public appeal() {
+		console.log(this.razonApelacion + "; " + this.descripcionApelacion);
 	}
 }
